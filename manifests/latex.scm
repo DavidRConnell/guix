1beabc1a486fbb8e4bb51e536735cd2ef211fd29
David R Connell <davidconnell12@gmail.com>
2021-03-04 00:14:42 -0600

    Add: packages for running ox-latex

--- manifests/latex.scm
+++ manifests/latex.scm
@@ -1,2 +1,13 @@
 (specifications->manifest
- (list "texlive"))
+ (list "texlive-base"
+       ;; Packages to get ox-latex running
+       "texlive-amsfonts"
+       "texlive-fonts-ec"
+       "texlive-xcolor"
+       "texlive-latex-pgf"
+       "texlive-latex-wrapfig"
+       "texlive-latex-hyperref"
+       "texlive-latex-capt-of"
+       "texlive-generic-ulem"
+       ;; Other packages
+       "texlive-latex-natbib"))
