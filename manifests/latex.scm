2c8481180acce5e252899ab0d930df853af98583
David R Connell <davidconnell12@gmail.com>
2021-03-04 23:02:16 -0600

    Change: to tectonic

--- manifests/latex.scm
+++ manifests/latex.scm
@@ -1,13 +1,2 @@
 (specifications->manifest
- (list "texlive-base"
-       ;; Packages to get ox-latex running
-       "texlive-amsfonts"
-       "texlive-fonts-ec"
-       "texlive-xcolor"
-       "texlive-latex-pgf"
-       "texlive-latex-wrapfig"
-       "texlive-latex-hyperref"
-       "texlive-latex-capt-of"
-       "texlive-generic-ulem"
-       ;; Other packages
-       "texlive-latex-natbib"))
+ (list "tectonic"))
